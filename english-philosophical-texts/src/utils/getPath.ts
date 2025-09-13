b637e4fe6c2225a166fa10eff5dd9bfb15dc3c4b
Amyas Merivale <merivale@gmail.com>
2025-09-13 20:02:10 +0200

    Make texts directory customisable with an env variable

--- english-philosophical-texts/src/utils/getPath.ts
+++ english-philosophical-texts/src/utils/getPath.ts
@@ -1,10 +1,14 @@
 import { exists } from "@std/fs";
 
+const textsDir = Deno.env.get("TEXTS_DIR") ?? "./texts";
+
 export default async (id: string): Promise<string | undefined> => {
   const idPath = id.replaceAll(".", "/").toLowerCase();
-  if (await exists(`texts/${idPath}.mit`)) return `texts/${idPath}.mit`;
-  if (await exists(`texts/${idPath}/index.mit`)) {
-    return `texts/${idPath}/index.mit`;
+  if (await exists(`${textsDir}/${idPath}.mit`)) {
+    return `${textsDir}/${idPath}.mit`;
+  }
+  if (await exists(`${textsDir}/${idPath}/index.mit`)) {
+    return `${textsDir}/${idPath}/index.mit`;
   }
   return undefined;
 };
