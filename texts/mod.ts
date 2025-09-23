a12e1771185bf93a25821ca66feedd454a9af6f9
Amyas Merivale <merivale@gmail.com>
2025-09-23 09:11:47 +0200

    Export getAuthor from texts service

--- texts/mod.ts
+++ texts/mod.ts
@@ -1,3 +1,4 @@
+export { default as getAuthor } from "./src/getAuthor.ts";
 export { default as getIndex } from "./src/getIndex.ts";
 export { default as getPath } from "./src/getPath.ts";
 export { default as getText } from "./src/getText.ts";
