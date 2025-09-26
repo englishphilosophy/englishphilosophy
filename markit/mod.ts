31555a2738c75585a0933009c1d23f45b75d6519
Amyas Merivale <merivale@gmail.com>
2025-09-26 16:15:17 +0200

    Fix linting and formatting issues

--- markit/mod.ts
+++ markit/mod.ts
@@ -1,4 +1,7 @@
 export { default as compile } from "./src/compile.ts";
-export { default as content, innerContent } from "./src/lib/compile/blocks/content.ts"
+export {
+  default as content,
+  innerContent,
+} from "./src/lib/compile/blocks/content.ts";
 export * from "./src/types.ts";
 export { default as validate } from "./src/validate.ts";
