f9f52276d4b940c6569e394d2196ab0f0a4dab8a
Amyas Merivale <merivale@gmail.com>
2025-09-26 16:17:16 +0200

    Fix more formatting issues

--- texts/src/getText.ts
+++ texts/src/getText.ts
@@ -1,12 +1,12 @@
 import { normalize } from "@englishphilosophy/dictionary";
-import { compile, innerContent, type Format } from "@englishphilosophy/markit";
+import { compile, type Format, innerContent } from "@englishphilosophy/markit";
 import getPath, { textsDir } from "./getPath.ts";
 import type { Text, TextStub } from "./types.ts";
 
 export default async (
   id: string,
   format: Format = "html",
-  normalized = false
+  normalized = false,
 ): Promise<Text | undefined> => {
   const path = await getPath(id);
 
@@ -30,17 +30,17 @@ export default async (
 
 const formatTextFields = <Type extends Text | TextStub>(
   text: Type,
-  format: Format
+  format: Format,
 ): Type => {
   if (text.title) {
-    text.title =
-      format === "markit" ? text.title : innerContent(text.title, format);
+    text.title = format === "markit"
+      ? text.title
+      : innerContent(text.title, format);
   }
   if (text.sourceDesc) {
-    text.sourceDesc =
-      format === "markit"
-        ? text.sourceDesc
-        : innerContent(text.sourceDesc, format);
+    text.sourceDesc = format === "markit"
+      ? text.sourceDesc
+      : innerContent(text.sourceDesc, format);
   }
   if ("ancestors" in text) {
     text.ancestors = text.ancestors.map((ancestor) =>
