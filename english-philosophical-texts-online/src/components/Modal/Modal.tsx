8dcf3e176bf682ddf1e1c965078d97da4101455d
Amyas Merivale <merivale@gmail.com>
2026-01-24 10:20:24 +0100

    Add explanatory comment to future self

--- english-philosophical-texts-online/src/components/Modal/Modal.tsx
+++ english-philosophical-texts-online/src/components/Modal/Modal.tsx
@@ -10,6 +10,8 @@ export default ({ id, title, children }: Props) => (
   <dialog id={id} class="modal-wrapper">
     <div class="modal">
       <header>
+        {/* sometimes the title comes from Markit files compiled to HTML,
+         /* so using dangerouslySetInnerHTML here instead of children */}
         <h3 dangerouslySetInnerHTML={{ __html: title }} />
         <button type="button" class="close-modal-button" data-close={id}>
           &times;
